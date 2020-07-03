94784d4f279a4347a4d8ee0db01dcddabe70711a
Joel Aldave <aldavecueva@gmail.com>
2020-07-02 20:54:39 -0400

    docs agregados

---
+++ docs/precache-manifest.49ea989ca97c22590e6f920e9cc047ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "204530967ccd67c12f35783d4bb43dd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b46d6be08a75f82a669",
+    "url": "/static/css/main.e839028a.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b46d6be08a75f82a669",
+    "url": "/static/js/main.6a792b33.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
